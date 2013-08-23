a5ae65fea3e254f624324cb0e7d7bf55c389dbe2
howthebodyworks <git@email.possumpalace.org>
2013-08-23 15:11:33 +0200

    first round of updates to run at ICST - removing deps, improving docs

--- f_lustre.sh
+++ f_lustre.sh
@@ -1,4 +1,6 @@
-#!/bin/env sh
+#!/usr/bin/env sh
+##!/usr/bin/env PATH="$PATH":/Applications/SuperCollider/SuperCollider.app/Contents/Resources/sclang sh
+export PATH=$PATH:/Applications/SuperCollider/SuperCollider.app/Contents/Resources/
 sclang -d $PWD settings_icst.scd f_lustre.scd
 #~/processing-java --sketch=$PWD/f_lustre_display --output=$PWD/output --force --present width=80
 #https://forum.processing.org/topic/use-external-editor-is-gone-in-beta-5-now-what
