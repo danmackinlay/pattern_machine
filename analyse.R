af1c701f0b43705897400fffc9d96dd142b0bccc
howthebodyworks <git@email.possumpalace.org>
2014-04-03 05:31:40 +0200

    cross-validated fit using the glmnet package

--- analyse.R
+++ analyse.R
@@ -1,3 +1,5 @@
+require("glmnet")
+
 notes = read.csv("dillpick.csv", header=TRUE)
 
 # neg log likelihood cost
@@ -6,7 +8,7 @@ binomial.cost <- function(r, pi=0) {
 #   print(r);
 #   print("pi");
 #   print(pi);
-  return(mean(-log(r*pi+(1-r)*(1-pi))))
+  return(sum(-log(r*pi+(1-r)*(1-pi))))
 }
 # Is this better than neg log likelihood?
 
@@ -18,6 +20,19 @@ notes.off$totalHeld=rowSums(notes.off[substr(names(notes.off),1,1)=="X"])
 notes.off = subset(notes.off, totalHeld>1)
 notes.off$totalHeld = NULL
 
+notes.off.successes = notes.off[rep(row.names(notes.off), notes.off$ons),]
+notes.off.successes$ons=NULL
+notes.off.successes$offs=NULL
+notes.off.successes$result=1
+notes.off.fails = notes.off[rep(row.names(notes.off), notes.off$offs),]
+notes.off.fails$ons=NULL
+notes.off.fails$offs=NULL
+notes.off.fails$result=0
+notes.off = rbind(notes.off.successes, notes.off.fails)
+rm(notes.off.fails)
+rm(notes.off.successes)
+
+
 #data to fit the note ADDITION mode.
 notes.on = notes[notes$X0==1,]
 notes.on[names(notes.on)=="X0"] = NULL
@@ -26,8 +41,6 @@ notes.on$totalHeld=rowSums(notes.on[substr(names(notes.on),1,1)=="X"])
 notes.on = subset(notes.on, totalHeld>0)
 notes.on$totalHeld = NULL
 
-#TODO: regularized fit
-#probably glmnet
 notes.on.successes = notes.on[rep(row.names(notes.on), notes.on$ons),]
 notes.on.successes$ons=NULL
 notes.on.successes$offs=NULL
@@ -40,29 +53,7 @@ notes.on = rbind(notes.on.successes, notes.on.fails)
 rm(notes.on.fails)
 rm(notes.on.successes)
 
-notes.off.successes = notes.on[rep(row.names(notes.on), notes.on$ons),]
-notes.off.successes$ons=NULL
-notes.off.successes$offs=NULL
-notes.off.successes$result=1
-notes.off.fails = notes.on[rep(row.names(notes.on), notes.on$offs),]
-notes.off.fails$ons=NULL
-notes.off.fails$offs=NULL
-notes.off.fails$result=0
-notes.off = rbind(notes.off.successes, notes.off.fails)
-rm(notes.off.fails)
-rm(notes.off.successes)
+fit.on = cv.glmnet(as.matrix(notes.on[,-(ncol(notes.on))]),notes.on[,(ncol(notes.on))], family="binomial", alpha=1)
+fit.off = cv.glmnet(as.matrix(notes.off[,-(ncol(notes.off))]),notes.off[,(ncol(notes.off))], family="binomial", alpha=1)
 
-fit.on = glm(result~(X.6 + X.5 + X.4 + X.3 + X.2 + X.1 + X1 + X2 + X3 +X4 + X5 +X6)^2, data=notes.on, family="binomial")
-fit.off = glm(result~(X.6 + X.5 + X.4 + X.3 + X.2 + X.1 + X1 + X2 + X3 +X4 + X5 +X6)^2, data=notes.off, family="binomial")
-
-# both these are LOOCV
-# fit.on.cv.err <- cv.glm(notes.on, fit.on, binomial.cost, K=nrow(notes.on))$delta
-# fit.on.cv.err <- cv.glm(notes.on, fit.on, binomial.cost)$delta
-# 11-fold CV, FWIW
-fit.on.cv.11.err <- cv.glm(notes.on, fit.on, binomial.cost, K=11)$delta
-
-# However, models fit to the binomial responses instead of boolean responses have 
-# defective cross validation - every probability has the same weight regardless of
-# how many samples it has. Nasty.
-
-glm.diag.plots(fit.on,glm.diag(fit.on))
+#glm.diag.plots(fit.on,glm.diag(fit.on))
