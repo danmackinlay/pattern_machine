4df1ffae8ae4be2b737138120cd3c92621407f28
howthebodyworks <git@email.possumpalace.org>
2014-04-07 14:34:45 +0200

    large cross-validated model. Too slow. Terrible.

--- analyse.R
+++ analyse.R
@@ -1,6 +1,6 @@
 require("penalized")
 
-# Am I doing this wrong? I could model odds of each note going on conditional on environment.
+# Am I doing this wrong? I could model odds of each note sounding conditional on environment.
 # Could also model, conditional on environemtn, which note goes on.
 # More tractable, I could condition for note-on probabilities given the *number* of simultaneous notes
 # this would possibly more interpretable.
@@ -12,11 +12,18 @@ require("penalized")
 # nice vignette: http://nlp.stanford.edu/manning/courses/ling289/logistic.pdf
 # or traditional AIC style: http://data.princeton.edu/R/glms.html
 
-notes = read.csv("dillpick.csv", header=TRUE)
+#if this DOESN'T work, could go to a discrete PGM model, such as
+# http://cran.r-project.org/web/packages/catnet/vignettes/catnet.pdf
+# https://r-forge.r-project.org/R/?group_id=1487
+# gRaphHD http://www.jstatsoft.org/v37/i01/
+# http://www.bnlearn.com/
+# but let's stay simple.
+
+source.notes = read.csv("dillpick.csv", header=TRUE)
 
 # data to fit the note model, GIVEN THE CURRENT NOTE IS OFF
-# i.e. the note addition model
-notes.off = notes[notes$X0==0,]
+# i.e. the note ADDITION model
+notes.off = source.notes[source.notes$X0==0,]
 notes.off[names(notes.off)=="X0"] = NULL
 notes.off.predictor.names = colnames(notes.off)[substr(names(notes.off),1,1)=="X"]
 notes.off$totalHeld=rowSums(notes.off[notes.off.predictor.names])
@@ -53,25 +60,25 @@ notes.off.interactions = apply(
   })
 colnames(notes.off.interactions) = notes.off.interaction.strings
 
-# Finding an optimal cross-validated likelihood
-notes.off.opt = optL1(
-  response=notes.off$response,
-  penalized=as.formula(paste("~(", paste(notes.off.predictor.names, collapse="+"), ")^2")),
-  data=notes.off, model="logistic", trace=TRUE, fold = 10)
-plot(notes.off.opt$predictions)
-
-# Plotting the profile of the cross-validated likelihood
-notes.off.prof <- profL1(
-  response=notes.off$response,
-  penalized=as.formula(paste("~(", paste(notes.off.predictor.names, collapse="+"), ")^2")),
-  trace=TRUE, 
-  fold = notes.off.opt$fold, steps=20)
-plot(notes.off.prof$lambda, notes.off.prof$cvl, type="l")
-plotpath(notes.off.prof$fullfit)
+# # Finding an optimal cross-validated likelihood
+# notes.off.opt = optL1(
+#   response=notes.off$response,
+#   penalized=as.formula(paste("~(", paste(notes.off.predictor.names, collapse="+"), ")^2")),
+#   data=notes.off, model="logistic", trace=TRUE, fold = 10)
+# plot(notes.off.opt$predictions)
+# 
+# # Plotting the profile of the cross-validated likelihood
+# notes.off.prof <- profL1(
+#   response=notes.off$response,
+#   penalized=as.formula(paste("~(", paste(notes.off.predictor.names, collapse="+"), ")^2")),
+#   data=notes.off, fold = notes.off.opt$fold, steps=20,
+#   trace=TRUE)
+# plot(notes.off.prof$lambda, notes.off.prof$cvl, type="l")
+# plotpath(notes.off.prof$fullfit)
 
 #data to fit the note model, GIVEN THE CURRENT NOTE IS ON
 # i.e. note removal.
-notes.on = notes[notes$X0==1,]
+notes.on = source.notes[source.notes$X0==1,]
 notes.on[names(notes.on)=="X0"] = NULL
 notes.on.predictor.names = colnames(notes.on)[substr(names(notes.on),1,1)=="X"]
 notes.on$totalHeld=rowSums(notes.on[notes.on.predictor.name])
@@ -93,11 +100,11 @@ rm(notes.on.fails)
 rm(notes.on.successes)
 
 #data to fit the COMBINED model, for tracking consonance
-notes.predictor.names = colnames(notes)[substr(names(notes),1,1)=="X"]
-notes[substr(names(notes),1,1)=="X"]
-notes$totalHeld=rowSums(notes[substr(names(notes),1,1)=="X"])
+notes.predictor.names = colnames(source.notes)[substr(names(source.notes),1,1)=="X"]
+notes = source.notes
+notes$totalHeld=rowSums(source.notes[substr(names(source.notes),1,1)=="X"])
 #remove initial nodes
-notes = subset(notes, totalHeld>0)
+notes = subset(source.notes, totalHeld>0)
 notes$totalHeld = NULL
 
 notes.successes = notes[rep(row.names(notes), notes$ons),]
