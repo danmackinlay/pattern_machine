3cf9b6aacbed8e44c45da5b7857e1fffd0dde281
howthebodyworks <git@email.possumpalace.org>
2014-04-26 18:12:33 +0200

    generalised happy model fist

--- analyse.R
+++ analyse.R
@@ -1,6 +1,8 @@
 library("Matrix")
 #library("LiblineaR")
 library("glmnet")
+library("stringr")
+library("jsonlite")
 require(doMC)
 registerDoMC(cores=2)
 
@@ -32,9 +34,7 @@ case.scale.factor = 24
 source.notes = read.csv("rag-11.csv", header=TRUE)
 source.notes$file = as.factor(source.notes$file)
 
-note.log.model = function(notes.data, ...) {
-  notes.predictor.names = colnames(source.notes)[2:(length(colnames(source.notes))-2)]
-  notes.predictor.names = notes.predictor.names[notes.predictor.names != "X0"]
+note.log.model = function(notes.data, notes.formula, ...) {
   notes.successes = notes.data[rep(row.names(notes.data), round(notes.data$ons/case.scale.factor)),]
   notes.successes$ons=NULL
   notes.successes$offs=NULL
@@ -48,7 +48,6 @@ note.log.model = function(notes.data, ...) {
   rm(notes.successes)
   
   # the original data frame and formula
-  notes.formula = as.formula(paste("~(", paste(notes.predictor.names, collapse="+"), ")^2"))
   notes.predictors.sparse=sparse.model.matrix(notes.formula, notes.data)
   notes.response=as.matrix(notes.data$response)
   notes.fit = 0
@@ -66,35 +65,49 @@ note.log.model = function(notes.data, ...) {
   print(notes.fit.time)
   return(notes.fit)
 }
+export.formula <- function (coefs.matrix) {
+#cast to string for export
+#   coef.list = list()
+#   for (n in row.names(coefs.matrix)) {
+#     if (coefs.matrix[n,] !=0) coef.list[n] = coefs.matrix[n,]
+#   }
+  export.formula = "["
+  for (n in row.names(coefs.matrix)) {
+    if (coefs.matrix[n,] !=0) {
+      export.formula = paste(export.formula, "[")
+      coef.bit = strsplit(
+        str_replace_all(str_replace_all(n, "X",""), "[.]", "-"), ":"
+      )
+      export.formula = paste(
+        export.formula, 
+        paste(
+          coef.bit
+        ), sep=", "
+      )
+      export.formula = paste(export.formula, coefs.matrix[n,], sep=", ")
+      export.formula = paste(export.formula, "]")
+      export.formula = paste(export.formula, "]")
+      #coef.list[n] = coefs.matrix[n,]
+    }
+  }
+  export.formula = paste(export.formula, "]")
+  return(export.formula)
+}
 
 # data to fit the note model, GIVEN THE CURRENT NOTE IS OFF
 # i.e. the note ADDITION model
+# with the new onset-led model, this is the only fit of relevence.
 notes.off = source.notes[source.notes$X0==0,]
 notes.off[names(notes.off)=="X0"] = NULL
 notes.off.held.names = colnames(notes.off)[substr(names(notes.off),1,1)=="X"]
+#use all predictors
+#notes.off.predictor.names = colnames(source.notes)[2:(length(colnames(source.notes))-2)]
+#use only held notes
+notes.off.predictor.names  = notes.off.held.names
+notes.off.formula = as.formula(paste("~(", paste(notes.off.predictor.names, collapse="+"), ")^2"))
+
 notes.off$totalHeld=rowSums(notes.off[notes.off.held.names])
 #remove initial nodes - i.e. there has to be one other note in range for this note to switch on
 notes.off = subset(notes.off, totalHeld>0)
 notes.off$totalHeld = NULL
-notes.off.fit = note.log.model(notes.off)
 
-#data to fit the note model, GIVEN THE CURRENT NOTE IS ON
-# i.e. note removal.
-notes.on = source.notes[source.notes$X0==1,]
-notes.on[names(notes.on)=="X0"] = NULL
-notes.on.held.names = colnames(notes.on)[substr(names(notes.on),1,1)=="X"]
-notes.on$totalHeld=rowSums(notes.on[notes.on.held.names])
-#remove terminal nodes
-# I.e. this note has to be interacting with at least one other note for us to care if it goes off
-notes.on = subset(notes.on, totalHeld>0)
-notes.on$totalHeld = NULL
-notes.on.fit = note.log.model(notes.on)
-
-# #data to fit the COMBINED model, for tracking consonance
-# notes.all.held.names = colnames(source.notes)[substr(names(source.notes),1,1)=="X"]
-# notes.all = source.notes
-# notes.all$totalHeld=rowSums(source.notes[substr(names(source.notes),1,1)=="X"])
-# #remove initial nodes
-# notes.all = subset(notes.all, totalHeld>0)
-# notes.all$totalHeld = NULL
-# notes.all.fit = note.log.model(notes.all)
