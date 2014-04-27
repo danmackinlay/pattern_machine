0e4991e04a9ad598c2e6994f3e679f0a65de9c00
howthebodyworks <git@email.possumpalace.org>
2014-04-27 17:53:36 +0200

    refactor into functions so I can codegen

--- tidy_up_r.py
+++ tidy_up_r.py
@@ -9,22 +9,28 @@ Outputs as JSON - but why not code generate for SC? life is short.
 
 from json import dump, load
 
-r_model = load(open("coef-11.json"))
-predictor_pairs = [[[], r_model.pop(u'(Intercept)')[0]]]
+def tidy_json_model(file_path):
+    r_model = load(open(file_path))
+    predictor_pairs = [[[], r_model.pop(u'(Intercept)')[0]]]
 
-for this_predictor_string, [this_coef] in r_model.iteritems():
-    print "*", this_predictor_string, this_coef
-    this_predictor_chunks = this_predictor_string.split(":")
-    this_predictor_refs = []
-    for chunk in this_predictor_chunks:
-        if not chunk.startswith("X"):
-            continue
-        chunk = chunk[1:]
-        if chunk.startswith("."):
-            ref = -int(chunk[1:])
-        else:
-            ref = int(chunk)
-        this_predictor_refs.append(ref)
-    predictor_pairs.append([this_predictor_refs,this_coef])
+    for this_predictor_string, [this_coef] in r_model.iteritems():
+        print "*", this_predictor_string, this_coef
+        this_predictor_chunks = this_predictor_string.split(":")
+        this_predictor_refs = []
+        for chunk in this_predictor_chunks:
+            if not chunk.startswith("X"):
+                continue
+            chunk = chunk[1:]
+            if chunk.startswith("."):
+                ref = -int(chunk[1:])
+            else:
+                ref = int(chunk)
+            this_predictor_refs.append(ref)
+        predictor_pairs.append([this_predictor_refs,this_coef])
+    return(predictor_pairs)
 
+def write_json_model(model, path):
+    with open(path, "w") as f:
+        dump(model, f)
 
+model = tidy_json_model("coef-11.json")
