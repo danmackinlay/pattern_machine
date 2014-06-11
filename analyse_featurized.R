d11af09561e622bc4404ba1e5abb0a577827934e
howthebodyworks <git@email.possumpalace.org>
2014-06-11 15:22:41 +0200

    non-crashy prallelism

--- analyse_featurized.R
+++ analyse_featurized.R
@@ -1,19 +1,35 @@
 library("Matrix")
 library("glmnet")
-library("stringr")
-#require(doMC)
-#registerDoMC(cores=4)
+
+# # crashes with index error
+# require(doMC)
+# registerDoMC(cores=4)
+
+# # crashes with index error
+# require(doParallel)
+# registerDoParallel(cores=4)
+
+# # ?
+# require(doParallel)
+# cl <- makeCluster(2)
+# registerDoParallel(cl)
+
+# works; could be faster
+require(doSNOW)
+cl <- makeCluster(4)
+registerDoSNOW(cl)
+
 require(rhdf5)
 source("config.R")
 
 #load actual data
-notes.obsdata = h5read(h5.file.name.basic, "/obs_meta")
+notes.obsdata = h5read(h5.file.name.from.python, "/obs_meta")
 notes.obsdata$file = as.factor(notes.obsdata$file)
-notes.obsidx = as.vector(h5read(h5.file.name.basic, '/v_feature_indices'))
-notes.obsptr = as.vector(h5read(h5.file.name.basic, '/v_feature_indptr'))
-notes.vals = as.vector(h5read(h5.file.name.basic, '/v_feature_data'))
-notes.dims = as.vector(h5read(h5.file.name.basic, '/v_feature_datadims'))
-notes.colnames = h5read(h5.file.name.basic, "/v_feature_col_names")
+notes.obsidx = as.vector(h5read(h5.file.name.from.python, '/v_feature_indices'))
+notes.obsptr = as.vector(h5read(h5.file.name.from.python, '/v_feature_indptr'))
+notes.vals = as.vector(h5read(h5.file.name.from.python, '/v_feature_data'))
+notes.dims = as.vector(h5read(h5.file.name.from.python, '/v_feature_datadims'))
+notes.colnames = h5read(h5.file.name.from.python, "/v_feature_col_names")
 
 notes.f = sparseMatrix(
   i=notes.obsidx,
@@ -33,8 +49,8 @@ notes.fit.time = system.time( #note this only works for <- assignment!
     family="binomial",
     type.logistic="modified.Newton", #speed-up, apparently.
     alpha=1,
-    dfmax=1000,
-    #parallel=TRUE,
+    #dfmax=200,
+    parallel=TRUE,
     foldid=ceiling(unclass(notes.obsdata$file)/3.4)
   )
 )
