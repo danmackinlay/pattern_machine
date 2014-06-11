cbae5c9c26899fa810ab88e3ac9c82697fa2ebbc
howthebodyworks <git@email.possumpalace.org>
2014-06-11 10:12:20 +0200

    delete unused nonsense

--- data_utils.R
+++ data_utils.R
@@ -1,37 +1,6 @@
 library("Matrix")
 require(rhdf5)
 
-trim.col = function(mat,n=0){
-  trimmed = mat[,(n+1):(ncol(mat)-n)]
-  mask = rowSums(mat)>0
-  return(list(trimmed=trimmed[mask,], mask = mask))
-}
-
-dissect.coefs = function(coefs){
-  #horrifically inefficient, but I can't be arsed working out how to do this better in R
-  #it's not conceptually well-posed anyway
-  scoefmags = (coefs[summary(coefs)$i])[-1]
-  coefmags = abs(scoefmags)
-  coefnames = rownames(coefs)[summary(coefs)$i][-1]
-  coeford = order(coefmags, decreasing=T)
-  coefchunks = data.frame()
-  for (i in 1:length(coefnames)) {
-    for (j in as.vector(str_split(coefnames[[i]], "[:x]")[[1]])) {
-      coefchunks = rbind(coefchunks, data.frame(name = j, mag=coefmags[i]))
-    }
-  }
-  coefchunks$name = as.factor(coefchunks$name)
-  coefsumm = data.frame()
-  for (j in levels(coefchunks$name)) {coefsumm = rbind(coefsumm, data.frame(name=j, mag = sum(coefchunks[coefchunks$name==j,"mag"])))}
-  coefsumm=coefsumm[order(coefsumm$mag,decreasing=T),]
-  return(list(
-    mag.imp=data.frame(
-      mag=scoefmags[coeford],
-      name=as.factor(coefnames[coeford])), 
-    chunks = coefsumm)
-  )
-}
-
 #triangular feature fn
 feat.tri = function(col, x0=1.0, radius=0.25) {
   return(pmax(1.0-abs((col-x0)/radius),0))
