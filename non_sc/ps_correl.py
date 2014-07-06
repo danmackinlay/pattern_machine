6292137248c4fc6e688deba6be6d9cef823c5a11
howthebodyworks <git@email.possumpalace.org>
2014-07-07 00:46:15 +0200

    server analyser over net for real

--- non_sc/ps_correl.py
+++ non_sc/ps_correl.py
@@ -1,16 +1,11 @@
 """
-rapidly analyse a bunch of files for a particular autocorrelation profile
+Rapidly analyse a bunch of files for a particular autocorrelation profile
 
 For fractional sample delay, we could do cubic interpolation, or polyphase filtering:
 http://www.tau.ac.il/~kineret/amit/scipy_tutorial/
 http://hub.hku.hk/bitstream/10722/46311/1/71706.pdf?accept=1
 http://cnx.org/content/m11657/latest/
 http://mechatronics.ece.usu.edu/yqchen/dd/index.html
-
-TODO: kNN search on this data set:
-http://scikit-learn.org/stable/modules/neighbors.html#ball-tree
-TODO: OSC query server - basic PyOSC should be OK, given low IO load.
-https://bitbucket.org/arjan/txosc/wiki/Home
 
 To consider: should we highpass as the base f of the signal to reduce spurious bass "correlation"? (or is that OK? since it will select for similar spectral balances)
 In which case, should normalising be relative to filtered, or total, amplitude?
@@ -19,19 +14,17 @@ Also to consider: random frequencies? if so, how many? Or, e.g. 7/11/13-tone ste
 
 Also, what loss function? negative correlation is more significant than positive, for example...
 
-TODO: How do we detect inharmonic noise? Convolved with shuffled, or enveloped pink/white noise? This would have the bonus of reducing need for high pass
-TODO: adaptive masking noise floor
-TODO: live server bus setting (time index, estimated amplitude, file index)
-TODO: live server synth triggering
-TODO: live client feedback
-TODO: serialise analysis to disk ? (not worth it right now; analysis speed is negligible even unoptimised)
-TODO: handle updates per OSC
 TODO: confirm this RC function has correct frequency parameterization
 TODO: handle multiple files
-TODO: decimation is to neareset whole number ratio and therefore does not respect time exactly.
-TODO: dimension reduction
+TODO: adaptive masking noise floor
 TODO: estimate variance of analysis; e.g. higher when amp is low, or around major changes
 TODO: search ALSO on variance, to avoid spurious transient onset matches, or to at least allow myself to have such things
+TODO: live client feedback
+TODO: serialise analysis to disk ? (not worth it right now; analysis speed is negligible even unoptimised)
+TODO: How do we detect inharmonic noise? Convolved with shuffled, or enveloped pink/white noise? This would have the bonus of reducing need for high pass
+TODO: dimension reduction
+TODO: live server synth triggering
+TODO: decimation is to neareset whole number ratio and therefore does not respect time exactly.
 TODO: switch to Erik De Castro Lopo's libsamplerate to do the conversions; scipy's decimate could be better; there exist nice wrappers eg https://github.com/cournape/samplerate
 """
 from sklearn.neighbors import NearestNeighbors, KDTree, BallTree
@@ -40,6 +33,8 @@ from ps_correl_config import *
 from ps_correl_analyze import sf_anal
 
 wavdata = sf_anal(SF_PATH)
+all_corrs = wavdata['all_corrs']
+sample_times = wavdata['sample_times']
 tree = BallTree(wavdata['all_corrs'].T, metric='euclidean')
 
 client = OSCClient()
@@ -53,15 +48,17 @@ def user_callback(path, tags, args, source):
     idx = args[1]
     lookup = args[3:] #ignores the amplitude?
     indices = tree.query(lookup, k=10, return_distance=False)
-    print indices
-    indices = list(*sample_times[indices])
-    # could send server bus messages and client info messages
-    client.send(OSCMessage("/c_setn", 1, indices[0]))
+    print "i1", indices
+    times = list(*sample_times[indices])
+    print "i2", times
+    # send server bus messages
+    msg = OSCMessage("/c_setn")
+    msg.extend([12, 1, times[0]])
+    client.send(msg)
+    print "yay"
 
-# distances, indices = tree.query([1,1,1,1,1,1,1,1,1,1,1,1], k=10)
 server = OSCServer(("localhost", 36000), client=client, return_port=57110)
 server.addMsgHandler("/transect", user_callback )
 client.send( OSCMessage("/notify", 1 ) ) #subscribe to server stuff
 
-#client.send( OSCMessage("/quit") )
 
