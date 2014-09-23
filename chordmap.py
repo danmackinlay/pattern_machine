160f7f32c2cf9287d60c299b060c8d67a8951a5a
howthebodyworks <git@email.possumpalace.org>
2014-09-23 12:30:53 +0200

    better naming

--- chordmap.py
+++ chordmap.py
@@ -15,6 +15,7 @@ N_HARMONICS = 16
 KERNEL_WIDTH = 0.01 # less than this and they are the same note
 
 def cross_p_idx(n1, n2):
+    "poor-mans's nditer, written offline when i coulnd't look it up"
     return np.vstack([
         np.tile(np.arange(n1),n2),
         np.repeat(np.arange(n2), n1),
@@ -38,11 +39,11 @@ harm_idx = np.arange(N_HARMONICS)
 cross_harm_idx = cross_p_idx(N_HARMONICS, N_HARMONICS)
 
 
-def rect_kernel(nums, width=0.01):
+def kernel_fn(nums, width=0.01):
     "returns rect kernel product of points [f1, a1, f2, a2]"
     return (abs(nums[0]-nums[2])<width)*nums[1]*nums[3]
 
-def note_product(n1, n2, kernel_fn=rect_kernel):
+def note_product(n1, n2):
     "note-specific cross-product"
     harm_fs = np.vstack([
         note_harmonics[n1,:][cross_harm_idx[0]],
@@ -60,7 +61,7 @@ def make_chord(notes):
     chord_harm_energies = np.tile(base_energies, len(notes))
     return np.vstack([chord_harm_fs, chord_harm_energies])
 
-def chord_product(c1, c2, kernel_fn=rect_kernel):
+def chord_product(c1, c2):
     idx = cross_p_idx(c1.shape[1], c2.shape[1])
     harm_fs = np.vstack([
         c1[:,idx[0]],
@@ -79,24 +80,26 @@ def chord_dist(c1, c2):
         + chord_product(c2, c2)
     )
 
-def chord_product_from_i(ci1, ci2, kernel_fn=rect_kernel):
+def chord_product_from_chord_i(ci1, ci2):
     ci1 = int(ci1)
     ci2 = int(ci2)
     indices = (ci1, ci2)
-    if not indices in _chord_product_from_i_cache:
-        _chord_product_from_i_cache[indices]  = chord_product(
+    if not indices in _chord_product_from_chord_i_cache:
+        _chord_product_from_chord_i_cache[indices]  = chord_product(
             make_chord(chord_notes_from_ind(ci1)),
             make_chord(chord_notes_from_ind(ci2))
         )
-    return _chord_product_from_i_cache[indices] 
-_chord_product_from_i_cache = {}
+    return _chord_product_from_chord_i_cache[indices] 
+_chord_product_from_chord_i_cache = {}
 
-def chord_dist_from_i(ci1, ci2):
+def chord_dist_from_chord_i(ci1, ci2):
     "construct a chord distance from the chord inner product"
     print ci1, ci2
     return sqrt(
-        chord_product_from_i(ci1, ci1)
-        - 2 * chord_product_from_i(ci1, ci2)
-        + chord_product_from_i(ci2, ci2)
+        chord_product_from_chord_i(ci1, ci1)
+        - 2 * chord_product_from_chord_i(ci1, ci2)
+        + chord_product_from_chord_i(ci2, ci2)
+    )
+
     )
     
