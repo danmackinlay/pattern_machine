55ada0f2ea51d97c215c7c105aef3331b478fec7
howthebodyworks <git@email.possumpalace.org>
2014-09-24 17:09:59 +0200

    automatically load cached versions

--- chordmap.py
+++ chordmap.py
@@ -4,8 +4,9 @@
 # all harmonics wrapped to the octave
 # can construct a similarity by the inner product based on inner product of the the kernel-approximated vectors, then measuring ratio to mean inner norm; this is more-or-less the covariance of spectral energy
 # also could track kernel width per-harmonic; is this principled? "feels" right, but we'll see.
-# also, chould this be L1 instead of L2?
-# could I optimised by creating a basis of notes (yes)
+# Could do a straight nearness search off the distance matrix using ball tree
+# or cast to a basis of notes using a custom kernel
+# need to have this in terms of notes though
 
 import numpy as np
 from scipy.spatial.distance import squareform, pdist
@@ -13,6 +14,9 @@ from math import sqrt
 import tables
 import gzip
 import cPickle as pickle
+from sklearn.manifold import MDS
+from sklearn.decomposition import PCA, KernelPCA
+import os.path
 
 N_HARMONICS = 16
 KERNEL_WIDTH = 0.01 # less than this and they are the same note
@@ -40,6 +44,7 @@ note_harmonics = (((np.outer(base_fundamentals, base_harm_fs)-1.0)%1.0)+1)
 note_idx = np.arange(12, dtype="uint32")
 harm_idx = np.arange(N_HARMONICS)
 cross_harm_idx = cross_p_idx(N_HARMONICS, N_HARMONICS)
+chord_idx = np.arange(2**12).reshape(2**12,1)
 
 def binrotate(i, steps=1, lgth=12):
     "convenient to pack note strings to ints for performance"
@@ -55,6 +60,8 @@ def v_kernel_fn(f1, f2, a1, a2, widths=0.01):
 
 def chord_notes_from_ind(i):
     return np.asarray(np.nonzero(bit_unpack(i))[0], dtype="uint")
+def chord_ind_from_notes(i):
+    return np.asarray(np.nonzero(bit_unpack(i))[0], dtype="uint")
 
 def make_chord(notes):
     notes = tuple(sorted(notes))
@@ -66,6 +73,9 @@ def make_chord(notes):
         ])
     return _make_chord_cache[notes] 
 _make_chord_cache = {}
+if os.path.exists('_chord_map_cache_make_chords.gz'):
+    with gzip.open('_chord_map_cache_make_chords.gz', 'rb') as f:
+        _make_chord_cache = pickle.load(f)
 
 def v_chord_product(c1, c2):
     idx = cross_p_idx(c1.shape[1], c2.shape[1])
@@ -109,6 +119,10 @@ def v_chord_product_from_chord_i(ci1, ci2):
         #print "HIT", indices, prod
         return prod
 _v_chord_product_from_chord_i_cache = {}
+if os.path.exists('_chord_map_cache_products.gz'):
+    with gzip.open('_chord_map_cache_products.gz', 'rb') as f:
+        _v_chord_product_from_chord_i_cache = pickle.load(f)
+
 
 def v_chord_dist_from_chord_i(ci1, ci2):
     "construct a chord distance from the chord inner product"
@@ -122,44 +136,61 @@ def v_chord_dist_from_chord_i(ci1, ci2):
     )
 v_chord_dist_from_chord_i.callct = 0
 
-chords_i_dists = pdist(
-    np.arange(2**12).reshape(2**12,1),
-    v_chord_dist_from_chord_i
-)
-chords_i_dists_square =squareform(chords_i_dists)
+chords_i_dists_square = None
+chords_i_dists = None
+chords_i_products = None
+chords_i_products_square = None
+    
+if os.path.exists("dists.h5"):
+    with tables.open_file("dists.h5", 'w') as handle:
+        chords_i_dists = handle.get_node("/", 'v_dists').read()
+        chords_i_dists = handle.get_node("/", 'v_sq_dists').read()
+        chords_i_dists = handle.get_node("/", 'v_products').read()
+        chords_i_dists = handle.get_node("/", 'v_sq_products').read()
+else:
+    chords_i_dists = pdist(
+        chord_idx,
+        v_chord_dist_from_chord_i
+    )
+    chords_i_dists_square =squareform(chords_i_dists)
+    chords_i_products = pdist(
+        np.arange(2**12).reshape(2**12,1),
+        v_chord_product_from_chord_i
+    )
+    chords_i_products_square = squareform(chords_i_products)
 
-chords_i_products = pdist(
-    np.arange(2**12).reshape(2**12,1),
-    v_chord_product_from_chord_i
-)
-chords_i_products_square =squareform(chords_i_products)
+if not os.path.exists("dists.h5"):
+    with tables.open_file("dists.h5", 'w') as handle:
+        data_atom_type = tables.Float32Atom()
+        filt=tables.Filters(complevel=5)
+        handle.create_carray("/",'v_dists',
+            atom=data_atom_type, shape=chords_i_dists.shape,
+            title="dists",
+            filters=filt)[:] = chords_i_dists
+        handle.create_carray("/",'v_sq_dists',
+            atom=data_atom_type, shape=chords_i_dists_square.shape,
+            title="sq dists",
+            filters=filt)[:] = chords_i_dists_square
+        handle.create_carray("/",'v_products',
+            atom=data_atom_type, shape=chords_i_products.shape,
+            title="product",
+            filters=filt)[:] = chords_i_products
+        handle.create_carray("/",'v_sq_products',
+            atom=data_atom_type, shape=chords_i_products_square.shape,
+            title="sq products",
+            filters=filt)[:] = chords_i_products_square
 
-with tables.open_file("dists.h5", 'w') as handle:
-    data_atom_type = tables.Float32Atom()
-    filt=tables.Filters(complevel=5)
-    handle.create_carray("/",'v_dists',
-        atom=data_atom_type, shape=chords_i_dists.shape,
-        title="dists",
-        filters=filt)[:] = chords_i_dists
-    handle.create_carray("/",'v_sq_dists',
-        atom=data_atom_type, shape=chords_i_dists_square.shape,
-        title="sq dists",
-        filters=filt)[:] = chords_i_dists_square
-    handle.create_carray("/",'v_products',
-        atom=data_atom_type, shape=chords_i_products.shape,
-        title="product",
-        filters=filt)[:] = chords_i_products
-    handle.create_carray("/",'v_sq_products',
-        atom=data_atom_type, shape=chords_i_products_square.shape,
-        title="sq products",
-        filters=filt)[:] = chords_i_products_square
+if not os.path.exists('_chord_map_cache_products.gz'):
+    #this pickle is incredibly huge; dunno why
+    with gzip.open('_chord_map_cache_products.gz', 'wb') as f:
+        pickle.dump(_v_chord_product_from_chord_i_cache, f)
+if not os.path.exists('_chord_map_cache_products.gz'):
+    #this one is tiny
+    with gzip.open('_chord_map_cache_products.gz', 'wb') as f:
+        pickle.dump(_make_chord_cache, f)
 
-#this pickle is incredibly huge; dunno why
-with gzip.open('_chord_map_cache_products.gz', 'wb') as f:
-    pickle.dump(_v_chord_product_from_chord_i_cache, f)
-#this one is tiny
-with gzip.open('_chord_map_cache_make_chords.gz', 'wb') as f:
-    pickle.dump(_make_chord_cache, f)
+kpca = KernelPCA(n_components=None, kernel='precomputed', eigen_solver='auto', tol=0, max_iter=None)
+kpca_trans = kpca.fit_transform(chords_i_products_square) #feed the product matric directly in for precomputed case. 
 
-
-#class sklearn.manifold.MDS(n_components=2, metric=True, n_init=4, max_iter=300, verbose=0, eps=0.001, n_jobs=1, random_state=None, dissimilarity='euclidean')
+mds = MDS(n_components=3, metric=True, n_init=4, max_iter=300, verbose=1, eps=0.001, n_jobs=3, random_state=None, dissimilarity='precomputed')
+mds_trans = mds.fit_transform(chords_i_dists_square)
