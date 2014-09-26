f4f171f03ff987ea589cb775cd99939e6b81f918
howthebodyworks <git@email.possumpalace.org>
2014-09-26 15:45:55 +0200

    factor out graphics-depended shit

--- chordmap.py
+++ chordmap.py
@@ -24,8 +24,6 @@ import cPickle as pickle
 from sklearn.manifold import MDS
 from sklearn.decomposition import PCA, KernelPCA
 import os.path
-from matplotlib import pyplot as plt
-from colorsys import hsv_to_rgb
 
 N_HARMONICS = 16
 KERNEL_WIDTH = 0.01 # less than this and they are the same note
@@ -222,43 +220,3 @@ def get_mds(n_dims=3):
     #lin_mds_trans = nonlin_mds.fit_transform(chords_i_dists_square, init=lin_mds_trans)
     return lin_mds, lin_mds_trans
 
-# Colours are tricky; let's start by making a basis that colourises according to position on the cycle of 5ths.
-rgbmap = np.array([
-    hsv_to_rgb((i*7%12)/12.0, 1, 1.0/12) for i in xrange(12)
-])
-
-id_rgb = np.array([
-    rgbmap[chord_notes_from_ind(i),:].sum(0) for i in xrange(4096)
-])
-
-def plot_2d(trans):
-    n_dims = trans.shape[1]
-    for d in n_dims:
-        fig = plt.figure(d)
-        ax = plt.axes([0., 0., 1., 1.])
-        plt.scatter(
-            trans[:, d],
-            trans[:, (d+1) % n_dims],
-        s=20, c=id_rgb)
-    plt.show()
-
-
-def plot_3d(trans):
-    from mpl_toolkits.mplot3d import Axes3D
-    n_dims = trans.shape[1]
-    fig = plt.figure()
-    
-    for x in n_dims-2:
-        ax = plt.add_subplot(110+x, projection='3d')
-        y = (d+1) % n_dims
-        z = (d+2) % n_dims
-        plt.scatter(
-            trans[:, x],
-            trans[:, y],
-            trans[:, z],
-        s=20, c=id_rgb)
-        ax.set_xlabel('{0} ax'.format(x))
-        ax.set_ylabel('{0} ax'.format(y))
-        ax.set_zlabel('{0} ax'.format(z))
-         
-    plt.show()
---
+++ chordmap_vis.py
@@ -0,0 +1,42 @@
+from matplotlib import pyplot as plt
+from colorsys import hsv_to_rgb
+from mpl_toolkits.mplot3d import Axes3D
+
+# Colours are tricky; let's start by making a basis that colourises according to position on the cycle of 5ths.
+rgbmap = np.array([
+    hsv_to_rgb((i*7%12)/12.0, 1, 1.0/12) for i in xrange(12)
+])
+
+id_rgb = np.array([
+    rgbmap[chord_notes_from_ind(i),:].sum(0) for i in xrange(4096)
+])
+
+def plot_2d(trans):
+    n_dims = trans.shape[1]
+    for d in n_dims:
+        fig = plt.figure(d)
+        ax = plt.axes([0., 0., 1., 1.])
+        plt.scatter(
+            trans[:, d],
+            trans[:, (d+1) % n_dims],
+        s=20, c=id_rgb)
+    plt.show()
+
+def plot_3d(trans):
+    n_dims = trans.shape[1]
+    fig = plt.figure()
+    
+    for x in n_dims-2:
+        ax = plt.add_subplot(110+x, projection='3d')
+        y = (d+1) % n_dims
+        z = (d+2) % n_dims
+        plt.scatter(
+            trans[:, x],
+            trans[:, y],
+            trans[:, z],
+        s=20, c=id_rgb)
+        ax.set_xlabel('{0} ax'.format(x))
+        ax.set_ylabel('{0} ax'.format(y))
+        ax.set_zlabel('{0} ax'.format(z))
+         
+    plt.show()
